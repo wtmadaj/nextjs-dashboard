a9d39c6491597443ad650c73ab20ade8b841c453
Wes Madaj <wtmadaj@gmail.com>
2024-06-01 20:59:25 -0400

    Up to Chapter 12 of nextjs tutorial

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,12 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Oswald, Lato, Montserrat, Public_Sans } from 'next/font/google';
+import {undefined} from "zod";
 
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
 });
+export const oswald = Oswald({ subsets: ['latin'] });
+export const lato = Lato({weight: ['400','700'], subsets: ['latin'] });
+export const montserrat = Montserrat({ subsets: ['latin'] })
+export const publicSans = Public_Sans({ subsets: ['latin'] })
